56ee5894237d899938202ac5ab496b7139d96758
tunawillow <97710706+tunawillow@users.noreply.github.com>
2022-07-28 16:22:44 -0400

    Update streamlit_ra_visualanalytics.py
    database csv must be uploaded by user

--- streamlit_ra_visualanalytics.py
+++ streamlit_ra_visualanalytics.py
@@ -23,44 +23,7 @@ plt.rc('ytick', labelsize=SMALL_SIZE)    # fontsize of the tick labels
 plt.rc('legend', fontsize=SMALL_SIZE)    # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
 
-st.title('LEXEO Regulatory Affairs: Correspondence Database')
-
-### load database file
-
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = pd.read_csv("Correspondence database.csv")
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done! (using st.cache)")
-
-
-### display raw data
-st.subheader('Regulatory Correspondence')
-st.write("Find the original excel file [here](https://lexeotx.sharepoint.com/:x:/r/sites/RegulatoryGroup/RG%20Administrative/Tina/Correspondence%20database.xlsx?d=wbd8eb268ac6f4de09b648ec42417b939&csf=1&web=1&e=J1RrNz).")
-'''
-This site is better used to make visual analytics. To look at specific correspondence, it is recommended to access the original excel file. 
-However, you can make basic searches here! To search for a keyword, click on cell in the table and use Ctrl+F to bring up the search bar :)
-'''
-st.write(data)
-
-### format page into 2 columns - 1 for pie chart, 1 for keywords 
-col1, col2 = st.columns([1, 1])
-
-### create piechart for column of interest  
-col1.header("Make pie chart analytics")
-
-# selectbox to choose column of interest
-pie_option = col1.selectbox(
-    'Create a pie chart for a column:',
-     data.columns)
-
-# piechart features
-pie_features = col1.checkbox('Display percentage')
-
-# make the piechart
-col1.subheader('Pie chart for ' + pie_option)
-
+### define functions
 def pieChart(option,features):
     # Display percentage? 
     if features:
@@ -73,38 +36,6 @@ def pieChart(option,features):
     plt.pie(pie, labels=pie.index,autopct=pct)
     col1.pyplot(fig)
     
-pieChart(pie_option,pie_features)
-
-
-### keyword bar graph 
-col2.header("Top Keywords")
-
-# separate keywords into list
-kw_unsplit = list(data["Keywords"])
-kw_list = []
-
-for element in kw_unsplit:
-    if type(element) == str:
-        kw_list += element.lower().split("; ")
-        
-# make dict of keywords and counts
-kw_dict = {}
-for kw in kw_list:
-    if kw in kw_dict:
-        kw_dict[kw] += 1
-    else:
-        kw_dict[kw] = 1
-
-# convert dict into dataframe
-kw_df = pd.DataFrame.from_dict(kw_dict, orient='index')
-kw_df.columns = ['count']
-
-# slide to display certain number of keywords
-kw_number = col2.slider('Display the top ___ keywords',min_value=1,max_value=20,value=10)
-
-# display count?
-display_count = col2.checkbox('Display keyword counts')
-
 # bar graph of top keywords   
 def keywords(x):
     kw_counted = kw_df.sort_values(by = ['count'],ascending=False).head(x)
@@ -115,4 +46,75 @@ def keywords(x):
         plt.bar_label(bar)
     col2.pyplot(fig)
     
-keywords(kw_number)
+    
+st.title('LEXEO Regulatory Affairs: Correspondence Database')
+
+st.subheader('Regulatory Correspondence')
+st.write("Find the original excel file [here](https://lexeotx.sharepoint.com/:x:/r/sites/RegulatoryGroup/RG%20Administrative/Tina/Correspondence%20database.xlsx?d=wbd8eb268ac6f4de09b648ec42417b939&csf=1&web=1&e=J1RrNz).")
+'''
+This site is better used to make visual analytics. To look at specific correspondence, it is recommended to access the original excel file. 
+However, you can make basic searches here! To search for a keyword, click on cell in the table and use CMD+F to bring up the search bar :)
+'''
+
+### upload database file
+file = st.file_uploader('Upload the database csv file here.')
+
+if file is not None:
+    # Create a text element and let the reader know the data is loading.
+    data_load_state = st.text('Loading data...')
+    # Load 10,000 rows of data into the dataframe.
+    data = pd.read_csv(file)
+    # Notify the reader that the data was successfully loaded.
+    data_load_state.text("Done! (using st.cache)")
+    st.write(data)
+
+    ### format page into 2 columns - 1 for pie chart, 1 for keywords 
+    col1, col2 = st.columns([1, 1])
+    
+    ### create piechart for column of interest  
+    col1.header("Make pie chart analytics")
+    
+    # selectbox to choose column of interest
+    pie_option = col1.selectbox(
+        'Create a pie chart for a column:',
+         data.columns)
+    
+    # piechart features
+    pie_features = col1.checkbox('Display percentage')
+    
+    # make the piechart
+    col1.subheader('Pie chart for ' + pie_option)
+        
+    pieChart(pie_option,pie_features)
+    
+    
+    ### keyword bar graph 
+    col2.header("Top Keywords")
+    
+    # separate keywords into list
+    kw_unsplit = list(data["Keywords"])
+    kw_list = []
+    
+    for element in kw_unsplit:
+        if type(element) == str:
+            kw_list += element.lower().split("; ")
+            
+    # make dict of keywords and counts
+    kw_dict = {}
+    for kw in kw_list:
+        if kw in kw_dict:
+            kw_dict[kw] += 1
+        else:
+            kw_dict[kw] = 1
+    
+    # convert dict into dataframe
+    kw_df = pd.DataFrame.from_dict(kw_dict, orient='index')
+    kw_df.columns = ['count']
+    
+    # slide to display certain number of keywords
+    kw_number = col2.slider('Display the top ___ keywords',min_value=1,max_value=20,value=10)
+    
+    # display count?
+    display_count = col2.checkbox('Display keyword counts')
+        
+    keywords(kw_number)
